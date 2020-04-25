6525659787be417f07961e84234a638d50cc61cd
stephen waller <stephenwaller@stephens-MacBook-Pro.local>
2020-04-25 14:46:39 -0600

    Fisrt Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Value.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            var value = await _context.Value.FindAsync(id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async void Post([FromBody] string value)
+        {
+            await _context.AddAsync(new Value { Name = value});
+            await _context.SaveChangesAsync(); 
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+  
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Value> Value { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder){
+
+            builder.Entity<Value>().HasData(
+
+                new Value {Id = 1, Name = "stephen"},
+                new Value {Id = 2, Name = "corey"},
+                new Value {Id = 3, Name = "waller"}
+            );
+
+            
+        }
+    }
+}
